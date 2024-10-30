85d0327c2d1582b3bb7dd208a66ebdcee593da7b
Maxim Zemtsov <Kamikadze1456@gmail.com>
2024-10-30 23:14:06 +0300

    3

--- src/tests/InMemoryTaskManagerTest.java
+++ src/tests/InMemoryTaskManagerTest.java
@@ -2,13 +2,11 @@ package tests;
 
 import manager.InMemoryTaskManager;
 import manager.TaskManager;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest {
